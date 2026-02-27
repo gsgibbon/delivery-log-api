cd2c140ba52562d861ad42511ec6bdda40630174
Guilherme Gibbon <gibbon309@hotmail.com>
2026-02-27 16:43:45 -0300

    refactor(users): add missing parentheses and fix controller scope

--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -2,7 +2,7 @@ import { UsersController } from "@/controllers/users-controller"
 import { Router } from "express"
 
 const usersRoutes = Router()
-const usersController = new UsersController
+const usersController = new UsersController()
 
 usersRoutes.post("/", usersController.create)
 
